12612aef2cb53287a2c8a3ad0a82590bad9508e4
EdelByte <edel-byte@proton.me>
2026-05-29 11:33:58 +0200

    refactor spring boot base library

--- src/main/kotlin/spring-boot-base.gradle.kts
+++ src/main/kotlin/spring-boot-base.gradle.kts
@@ -9,5 +9,9 @@ plugins {
 
 dependencies {
     implementation(platform("org.springframework.boot:spring-boot-dependencies:$SPRING_BOOT_VERSION"))
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
